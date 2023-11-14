41cac670431984675d523ee4c1434ed5b80c54a1
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-14 13:48:34 -0300

    feat: Implementa enum de tipo de renda

---
+++ src/main/java/com/arquiteture/domain/enums/TypeWageProvider.java
@@ -0,0 +1,33 @@
+package com.arquiteture.domain.enums;
+
+public enum TypeWageProvider {
+    INTERNSHIP(1, "Estágio"),
+    FREELANCER(2, "Freelancer"),
+    CONTRACT(3, "Contrato"),
+    CLT(4, "CLT"),
+    OTHER(5, "Outro");
+
+    private Integer id;
+    private String label;
+
+    TypeWageProvider(Integer id, String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
