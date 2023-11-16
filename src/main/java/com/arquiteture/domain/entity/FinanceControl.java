8dbd7d93146ec412a0558ed867227df56a10b098
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-16 17:43:31 -0300

    todo: Parei na escolha de um desing patern para implementar os gets da modelagem de FinanceControl.java

--- src/main/java/com/arquiteture/domain/entity/FinanceControl.java
+++ src/main/java/com/arquiteture/domain/entity/FinanceControl.java
@@ -21,9 +21,14 @@ public class FinanceControl extends BaseEntity {
     @Column(name = "finance_control_id")
     private String id;
 
-    @Column(name = "remunaration")
-    @OneToMany()
+    @OneToMany(targetEntity = Remuneration.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "finance_control")
     private List<Remuneration> remunerations;
+
+    @OneToMany(targetEntity = Expense.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "finance_control", orphanRemoval = true)
+    private List<Expense> expensesFixes;
+
+    @OneToMany(targetEntity = MonthlyContribution.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "finance_control", orphanRemoval = true)
+    private List<MonthlyContribution> monthlyContributions;
 
     @Override
     public String getId() {
