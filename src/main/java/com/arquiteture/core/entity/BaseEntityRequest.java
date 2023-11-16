4c76ad6a7de90904caa6d18037d14ad671d95632
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-16 16:38:48 -0300

    feat: Implementa Entidade base para representar os requestes

---
+++ src/main/java/com/arquiteture/core/entity/BaseEntityRequest.java
@@ -0,0 +1,15 @@
+package com.arquiteture.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BaseEntityRequest {
+
+    private String id;
+}
