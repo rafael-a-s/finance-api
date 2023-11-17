63847f8a34bff45b6c2495a0cff9b2b3ce1318c5
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-17 16:44:33 -0300

    fix: Concertanto mensagens de validação

--- src/main/java/com/arquiteture/domain/model/user/UserRequestDTO.java
+++ src/main/java/com/arquiteture/domain/model/user/UserRequestDTO.java
@@ -13,29 +13,29 @@ import org.hibernate.validator.constraints.br.CPF;
 @NoArgsConstructor
 public class UserRequestDTO {
 
-    @NotBlank(message = "validation.field.not-blank")
-    @NotNull(message = "validation.field.not-null")
-    @Size(message = "validation.user.firstName.size", max = 50, min = 3)
+    @NotBlank(message = "{validation.field.not-blank}")
+    @NotNull(message = "{validation.field.not-null}")
+    @Size(message = "{validation.user.firstName.size}", max = 50, min = 3)
     private String firstName;
 
-    @NotBlank(message = "validation.field.not-blank")
-    @NotNull(message = "validation.field.not-null")
-    @Size(message = "validation.user.lastName.size", max = 50, min = 3)
+    @NotBlank(message = "{validation.field.not-blank}")
+    @NotNull(message = "{validation.field.not-null}")
+    @Size(message = "{validation.user.lastName.size}", max = 50, min = 3)
     private String lastName;
 
-    @NotBlank(message = "validation.field.not-blank")
-    @NotNull(message = "validation.field.not-null")
-    @CPF(message = "validation.user.cpf.format")
+    @NotBlank(message = "{validation.field.not-blank}")
+    @NotNull(message = "{validation.field.not-null}")
+    @CPF(message = "{validation.user.cpf.format}")
     private String cpf;
 
-    @NotBlank(message = "validation.field.not-blank")
-    @NotNull(message = "validation.field.not-null")
-    @Email(message = "validation.user.email.format")
+    @NotBlank(message = "{validation.field.not-blank}")
+    @NotNull(message = "{validation.field.not-null}")
+    @Email(message = "{validation.user.email.format}")
     private String email;
 
-    @NotBlank(message = "validation.field.not-blank")
-    @NotNull(message = "validation.field.not-null")
-    @Size(message = "validation.user.password.size", max = 16, min = 8)
+    @NotBlank(message = "{validation.field.not-blank}")
+    @NotNull(message = "{validation.field.not-null}")
+    @Size(message = "{validation.user.password.size}", max = 16, min = 8)
     private String password;
 
 }
