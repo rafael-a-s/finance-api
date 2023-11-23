fd3eeab8a80426f1315da9489a937581567821ec
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-23 16:37:15 -0300

    feat: Add permit all no end point de login

--- src/main/java/com/arquiteture/resource/auth/AuthResource.java
+++ src/main/java/com/arquiteture/resource/auth/AuthResource.java
@@ -7,11 +7,11 @@ import com.arquiteture.domain.model.auth.AuthToken;
 import com.arquiteture.domain.model.auth.AuthUser;
 import com.arquiteture.domain.service.auth.AuthService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.MultivaluedMap;
-import org.jboss.resteasy.reactive.RestForm;
 
 @Path("/auth")
 public class AuthResource {
@@ -21,6 +21,7 @@ public class AuthResource {
 
     @POST
     @Path("/token")
+    @PermitAll
     @Consumes(MediaType.APPLICATION_JSON)
     public AuthUser token(AuthRequest request) throws DomainException, JsonProcessingException {
         return service.token(request);
