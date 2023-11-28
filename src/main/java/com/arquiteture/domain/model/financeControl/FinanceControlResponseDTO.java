8ee5c735d46b04435c4e6f262b3b8df085ee6d66
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-28 20:55:09 -0300

    feat: implementa response de finance control

--- src/main/java/com/arquiteture/domain/model/financeControl/FinanceControlResponseDTO.java
+++ src/main/java/com/arquiteture/domain/model/financeControl/FinanceControlResponseDTO.java
@@ -1,4 +1,27 @@
 package com.arquiteture.domain.model.financeControl;
 
+import com.arquiteture.domain.model.expense.ExpenseResponseDTO;
+import com.arquiteture.domain.model.monthlyContribution.MonthlyContributionResponseDTO;
+import com.arquiteture.domain.model.remuneration.RemunerationResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FinanceControlResponseDTO {
+
+    private String id;
+    private List<RemunerationResponseDTO> remunerations;
+    private List<ExpenseResponseDTO> expensesFixes;
+    private List<MonthlyContributionResponseDTO> monthlyContributions;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private boolean active;
 }
