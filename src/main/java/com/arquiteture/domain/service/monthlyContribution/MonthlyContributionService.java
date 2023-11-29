95d2b41d815be7ff2ef4a4ee418abe79ecdcc280
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-29 16:22:20 -0300

    fix: refatora método create

--- src/main/java/com/arquiteture/domain/service/monthlyContribution/MonthlyContributionService.java
+++ src/main/java/com/arquiteture/domain/service/monthlyContribution/MonthlyContributionService.java
@@ -1,19 +1,35 @@
 package com.arquiteture.domain.service.monthlyContribution;
 
+import com.arquiteture.core.exception.DomainException;
 import com.arquiteture.core.repository.IBaseRepository;
 import com.arquiteture.core.service.BaseService;
 import com.arquiteture.domain.entity.MonthlyContribution;
 import com.arquiteture.domain.repository.MonthlyContributionRepository;
+import com.arquiteture.domain.service.financeControl.IFinanceControlService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class MonthlyContributionService extends BaseService<MonthlyContribution> implements IMonthlyContributionService {
+
+    @Inject
+    IFinanceControlService financeControlService;
 
     protected MonthlyContributionService(final MonthlyContributionRepository repository) {
         super(repository);
     }
 
     public MonthlyContributionService() {}
+
+    @Override
+    public MonthlyContribution create(MonthlyContribution entity) throws DomainException {
+        validate(entity);
+        getRepository().persist(entity);
+
+        financeControlService.addMonthlyContribution(entity);
+
+        return entity;
+    }
 
     @Override
     public MonthlyContributionRepository getRepository() {
--- src/main/java/com/arquiteture/domain/service/remuneration/RemunerationService.java
+++ src/main/java/com/arquiteture/domain/service/remuneration/RemunerationService.java
@@ -1,19 +1,35 @@
 package com.arquiteture.domain.service.remuneration;
 
+import com.arquiteture.core.exception.DomainException;
 import com.arquiteture.core.repository.IBaseRepository;
 import com.arquiteture.core.service.BaseService;
 import com.arquiteture.domain.entity.Remuneration;
 import com.arquiteture.domain.repository.RemunerationRepository;
+import com.arquiteture.domain.service.financeControl.IFinanceControlService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class RemunerationService extends BaseService<Remuneration> implements IRemunerationService {
+
+    @Inject
+    IFinanceControlService financeControlService;
 
     protected RemunerationService (final IBaseRepository<Remuneration> repository){
         super(repository);
     }
 
     public RemunerationService(){}
+
+    @Override
+    public Remuneration create(Remuneration entity) throws DomainException {
+        validate(entity);
+        getRepository().persist(entity);
+
+        financeControlService.addRemuneration(entity);
+
+        return entity;
+    }
 
     @Override
     public RemunerationRepository getRepository() {
