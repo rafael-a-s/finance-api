5764f9beeef8a655049bf80c9489773e8400600e
rafael-a-s <rafaelaguiar@unitins.br>
2023-11-29 16:19:46 -0300

    fix: refatora método create

--- src/main/java/com/arquiteture/domain/service/expense/ExpenseService.java
+++ src/main/java/com/arquiteture/domain/service/expense/ExpenseService.java
@@ -5,6 +5,7 @@ import com.arquiteture.core.service.BaseService;
 import com.arquiteture.domain.entity.Expense;
 import com.arquiteture.domain.entity.TypeExpense;
 import com.arquiteture.domain.repository.ExpenseRepository;
+import com.arquiteture.domain.service.financeControl.IFinanceControlService;
 import com.arquiteture.domain.service.typeExpense.ITypeExpenseService;
 import com.arquiteture.domain.service.typeExpense.TypeExpenseService;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -13,11 +14,24 @@ import jakarta.inject.Inject;
 @ApplicationScoped
 public class ExpenseService extends BaseService<Expense> implements IExpenseService {
 
+    @Inject
+    IFinanceControlService financeControlService;
+
     protected ExpenseService(final ExpenseRepository repository, final ITypeExpenseService typeExpenseService) {
         super(repository);
     }
 
     public ExpenseService() {
+    }
+
+    @Override
+    public Expense create(Expense entity) throws DomainException {
+        validate(entity);
+        getRepository().persist(entity);
+
+        financeControlService.addExpense(entity);
+
+        return entity;
     }
 
     @Override
