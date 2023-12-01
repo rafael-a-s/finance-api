b5154d2e61e2994a0f9e279e94746e1d42b19a54
rafael-a-s <rafaelaguiar@unitins.br>
2023-12-01 13:30:26 -0300

    feat: Add finance control no reponse de user

--- src/main/java/com/arquiteture/domain/model/user/UserResponseDTO.java
+++ src/main/java/com/arquiteture/domain/model/user/UserResponseDTO.java
@@ -1,6 +1,7 @@
 package com.arquiteture.domain.model.user;
 
 import com.arquiteture.domain.enums.Roles;
+import com.arquiteture.domain.model.financeControl.FinanceControlResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ public class UserResponseDTO {
     private String cpf;
     private String email;
     private Set<Roles> roles;
+    private FinanceControlResponseDTO financeControl;
     private LocalDateTime createAt;
     private LocalDateTime updateAt;
 
