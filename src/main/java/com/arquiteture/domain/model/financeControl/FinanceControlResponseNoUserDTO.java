e3d7e06ea403ea16bdeb3e52a36a7c57ab875519
rafael-a-s <rafaelaguiar@unitins.br>
2024-02-12 15:46:17 -0300

    feat: create DTO sem loop

---
+++ src/main/java/com/arquiteture/domain/model/financeControl/FinanceControlResponseNoUserDTO.java
@@ -0,0 +1,27 @@
+package com.arquiteture.domain.model.financeControl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.arquiteture.domain.model.expense.ExpenseResponseDTO;
+import com.arquiteture.domain.model.monthlyContribution.MonthlyContributionResponseDTO;
+import com.arquiteture.domain.model.remuneration.RemunerationResponseDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FinanceControlResponseNoUserDTO {
+    private String id;
+    private List<RemunerationResponseDTO> remunerations;
+    private List<ExpenseResponseDTO> expensesFixes;
+    private List<MonthlyContributionResponseDTO> monthlyContributions;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    private boolean active;
+}
--- src/main/java/com/arquiteture/domain/model/user/UserResponseNoFinanceControlDTO.java
+++ src/main/java/com/arquiteture/domain/model/user/UserResponseNoFinanceControlDTO.java
@@ -1,30 +1,28 @@
 package com.arquiteture.domain.model.user;
 
-import com.arquiteture.domain.enums.Roles;
-import com.arquiteture.domain.model.financeControl.FinanceControlResponseNoUserDTO;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.Set;
+
+import com.arquiteture.domain.enums.Roles;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserResponseDTO {
-
+@Builder
+public class UserResponseNoFinanceControlDTO {
     private String id;
     private String firstName;
     private String lastName;
     private String cpf;
     private String email;
     private Set<Roles> roles;
-    private FinanceControlResponseNoUserDTO financeControl;
     private LocalDateTime createAt;
     private LocalDateTime updateAt;
 
     private boolean active;
-
 }
